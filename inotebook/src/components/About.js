fb8a130cbd0dedb4c6330f3236568b648b8f2f32
rohitkumawat7270 <109840424+rohitkumawat7270@users.noreply.github.com>
2022-11-17 17:26:59 +0530

    Final Commit

---
+++ inotebook/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="container my-3">
+      <h1 className="text-center my-5">About iNotebook</h1>
+      <div> iNotebook is a <strong>secure way</strong> to take notes. It stores all your notes and categorises them on the basis of the provided tags. Moreover, these notes are <strong>stored on the cloud!</strong> Hence, you can <strong>access them from anywhere</strong> and from any device you wish. Notes can be <strong> edited and deleted </strong> with just a single click.  The best part is ... this service is <strong>completely free!!</strong></div>
+    </div> 
+  )
+}
+
+export default About
